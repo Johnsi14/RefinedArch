ced559d813b7cd1a3c8c9d50605b5de37ecc131b
Jonas Ziegler <zieglerjonas14@gmail.com>
2024-03-09 15:47:36 +0100

    Added the Cleanup Script

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo rm -r out/
+sudo rm -r /tmp/archiso-tmp
+
+
+
+echo "##########################################"
+echo "All Cleaned Up"
+echo "##########################################"
