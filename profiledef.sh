50f86e93e17a587fb249d3be4991974f21a63f0e
Jonas Ziegler <zieglerjonas14@gmail.com>
2024-03-09 21:41:45 +0100

    Changed the Names to RefinedArch

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_name="refinedarch"
+iso_label="REARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="RefinedArch Linux <https://github.com/Johnsi14/RefinedArch>"
+iso_application="RefinedArch Linux Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
