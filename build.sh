b3a02f995b47d75435af4259ef321c13aa8e1eae
Jonas Ziegler <zieglerjonas14@gmail.com>
2024-03-20 22:33:08 +0100

    Made something for Signing the Repo

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+#Locally signing the Keys if they are not on the keyserver
+sudo pacman-key --recv-keys E13A47BF81B58F54 --keyserver hkps://keyserver.ubuntu.com
+sudo pacman-key --lsign-key E13A47BF81B58F54
+
 sudo mkarchiso -v -w /tmp/archiso-tmp ~/RefinedArch
