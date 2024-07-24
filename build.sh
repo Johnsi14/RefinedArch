ac9ad10890cbf31329e81d0b2f98c9b83fc17d33
Jonas Ziegler <zieglerjonas14@gmail.com>
2024-07-24 21:58:16 +0200

    Removed the RefinedRepo

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 sudo pacman -Sy
-#Locally signing the Keys if they are not on the keyserver
-sudo pacman-key --recv-keys 6A678B3B845F22B6CC88D847108D8B736681D3D0 --keyserver hkps://keyserver.ubuntu.com
-sudo pacman-key --lsign-key 6A678B3B845F22B6CC88D847108D8B736681D3D0
 
 sudo mkarchiso -v -w /tmp/archiso-tmp ~/RefinedArch
