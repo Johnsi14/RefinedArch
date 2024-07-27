e78be81c1d138970b93a84cea0ecf84c40ccd0dd
Jonas Ziegler <zieglerjonas14@gmail.com>
2024-07-27 09:56:11 +0200

    Maintenance after Programming Pause

--- test.sh
+++ test.sh
@@ -3,4 +3,6 @@
 # shellcheck disable=SC2012
 newest_file=$(ls -t out| head -1)
 
-run_archiso "-i$HOME/RefinedArch/out/$newest_file"
+run_archiso "-i$HOME/RefinedArch/out/$newest_file" >/dev/null 2>&1 &
+
+wait $!
